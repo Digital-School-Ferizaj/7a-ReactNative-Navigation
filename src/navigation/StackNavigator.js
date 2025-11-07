cf2a5780e4638d0ebc3a0c35bfc27cd5d646075b
lirakH <lirakhamiti@hotmail.com>
2025-11-07 20:32:27 +0100

    simlify init

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -2,36 +2,8 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from "../screens/HomeScreen";
 import AboutScreen from "../screens/aboutScreen";
-import ShopScreen from "../screens/shopScreen";
-import ListScreen from '../screens/listScreen';
-import ProductList from '../screens/productList';
-import SimProductList from '../screens/simProdList';
-import FlexScreen from '../screens/flexScreen';
-import ApiScreen from '../screens/apiScreen';
-import ChallengeScreen from '../screens/challengeScreen';
-import CountriesScreen from '../screens/countriesScreen';
 
 const Stack = createStackNavigator();
-
-const StackNavigator = () => {
-  return (
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{ headerTitle: 'App' }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="About" component={AboutScreen} />
-        <Stack.Screen name="Shop" component={ShopScreen} />
-        <Stack.Screen name="List" component={ListScreen} />
-        <Stack.Screen name="ProductList" component={ProductList} />
-        <Stack.Screen name="SimProductList" component={SimProductList} />
-        <Stack.Screen name="FlexScreen" component={FlexScreen} />
-        <Stack.Screen name="ApiScreen" component={ApiScreen} />
-        <Stack.Screen name="ChallengeScreen" component={ChallengeScreen} />
-        <Stack.Screen name="CountriesScreen" component={CountriesScreen} />
-      </Stack.Navigator>
-  );
-}
 
 const HomeStack = ({navigation}) => {
   return (
@@ -55,5 +27,4 @@ const screenOptionStyle = {
   headerTitleStyle: { fontWeight: 'bold' },
 };
 
-export {StackNavigator, HomeStack, AboutStack};
-export default StackNavigator;
+export {HomeStack, AboutStack};
